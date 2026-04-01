2280cfa55bb93b2e1a39abf40bced197d939ef0c
silaesme <silaesme@gmail.com>
2026-04-02 01:38:11 +0300

    first of the first

--- src/design-system/types.tsx
+++
@@ -1,6 +0,0 @@
-import { colors } from './tokens/colors';
-import { typography } from './tokens/typography';
-
-// Renk anahtarlarını tip olarak çıkartıyoruz (örn: 'primary' | 'secondary')
-export type ColorVariant = keyof typeof colors;
-export type TextVariant = keyof typeof typography.fontSize;
